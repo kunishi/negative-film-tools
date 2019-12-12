7cfc7cc0f88710ecc27e803bd62262186033eb1e
Takeo Kunishima <t.kunishi@gmail.com>
2019-12-13 07:08:16 +0900

    use cutoff when autocontrast.

--- python/test.py
+++ python/test.py
@@ -12,7 +12,8 @@ def gamma(gamma_r, gamma_g, gamma_b, gain_r=1.0, gain_g=1.0, gain_b=1.0):
     b_tbl = [min(255, int((x / 255.) ** (1. / gamma_b) * gain_b * 255.)) for x in range(256)]
     return r_tbl + g_tbl + b_tbl
 
-GAMMA = 1.15
+#GAMMA = 1.2 
+GAMMA = 1.0
 
 raw = rawpy.imread(sys.argv[1])
 rgb = raw.postprocess(use_auto_wb=True)
@@ -20,6 +21,6 @@ image = Image.fromarray(rgb)
 imageBox = image.getbbox()
 cropped = image.crop(imageBox)
 inverted = PIL.ImageOps.invert(cropped)
-contrasted = PIL.ImageOps.autocontrast(inverted)
+contrasted = PIL.ImageOps.autocontrast(inverted, cutoff=1)
 gamma_img = contrasted.point(gamma(GAMMA, GAMMA, GAMMA))
 gamma_img.save('out.tif')
