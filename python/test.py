4a31ccac6c4177e19e586082cf4c1d2432014aa6
Takeo Kunishima <t.kunishi@gmail.com>
2019-12-13 01:51:51 +0900

    gamma correction.

--- python/test.py
+++ python/test.py
@@ -4,13 +4,22 @@ import rawpy
 import imageio
 from PIL import Image
 import PIL.ImageOps
+import numpy as np
 
-raw = rawpy.imread("Film127_11.dng")
+def gamma(gamma_r, gamma_g, gamma_b, gain_r=1.0, gain_g=1.0, gain_b=1.0):
+    r_tbl = [min(255, int((x / 255.) ** (1. / gamma_r) * gain_r * 255.)) for x in range(256)]
+    g_tbl = [min(255, int((x / 255.) ** (1. / gamma_g) * gain_g * 255.)) for x in range(256)]
+    b_tbl = [min(255, int((x / 255.) ** (1. / gamma_b) * gain_b * 255.)) for x in range(256)]
+    return r_tbl + g_tbl + b_tbl
+
+GAMMA = 1.15
+
+raw = rawpy.imread(sys.argv[1])
 rgb = raw.postprocess(use_auto_wb=True)
-#inverted = (255 - rgb)
-#imageio.imsave('Film150_11.tiff', inverted)
-
 image = Image.fromarray(rgb)
-inverted = PIL.ImageOps.invert(image)
+imageBox = image.getbbox()
+cropped = image.crop(imageBox)
+inverted = PIL.ImageOps.invert(cropped)
 contrasted = PIL.ImageOps.autocontrast(inverted)
-contrasted.save('Film127_11.tiff')
+gamma_img = contrasted.point(gamma(GAMMA, GAMMA, GAMMA))
+gamma_img.save('out.tif')
