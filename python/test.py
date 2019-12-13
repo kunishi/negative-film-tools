87225b66066183f148d44e1b950bd07386965cae
Takeo Kunishima <t.kunishi@gmail.com>
2019-12-13 16:22:27 +0900

    16bit operations.

--- python/test.py
+++ python/test.py
@@ -13,14 +13,14 @@ def gamma(gamma_r, gamma_g, gamma_b, gain_r=1.0, gain_g=1.0, gain_b=1.0):
     b_tbl = [min(255, int((x / 255.) ** (1. / gamma_b) * gain_b * 255.)) for x in range(256)]
     return r_tbl + g_tbl + b_tbl
 
-def cv2_gamma(gamma):
-    table = np.empty((1, 256), np.uint8)
-    for i in range(256):
-        table[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
+def cv2_gamma(gamma, bit=8):
+    table = np.empty((1, 2 ** bit), np.uint16)
+    for i in range(2 ** bit):
+        table[0, i] = np.clip(pow(i / float(2 ** bit), gamma) * float(2 ** bit), 0, 2 ** bit)
     return table
 
 raw = rawpy.imread(sys.argv[1])
-rgb = raw.postprocess(gamma=(1, 1), no_auto_bright=False, use_camera_wb=False, use_auto_wb=True, output_bps=8)
+rgb = raw.postprocess(gamma=(1, 1), no_auto_bright=False, use_camera_wb=False, use_auto_wb=True, output_bps=16)
 
 # read image from array
 #image = Image.fromarray(rgb)
@@ -34,12 +34,18 @@ inverted = cv2.bitwise_not(image)
 #inverted = rgb
 
 # gamma correction
-GAMMA = 1.3
+GAMMA = 1.2
 #GAMMA = 1.0 
 #gamma_img = contrasted.point(gamma(GAMMA, GAMMA, GAMMA))
 #gamma_img.save('out.tif')
-rgb_gamma = cv2.LUT(inverted, cv2_gamma(GAMMA))
-#rgb_gamma = contrasted
+#rgb_gamma = cv2.LUT(inverted, cv2_gamma(GAMMA))
+lut_table = cv2_gamma(GAMMA, bit=16)
+rgb_gamma = np.empty_like(inverted)
+rangex, rangey, dim = rgb_gamma.shape
+for x in range(rangex):
+    for y in range(rangey):
+        rgb_gamma[x, y] = lut_table[0, inverted[x, y]]
+#rgb_gamma = inverted
 
 # auto contrast
 #contrasted = PIL.ImageOps.autocontrast(inverted, cutoff=1)
