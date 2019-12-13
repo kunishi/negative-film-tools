3842a403c8f39bdf8bd13152837b16f2be5f2ac2
Takeo Kunishima <t.kunishi@gmail.com>
2019-12-14 08:25:19 +0900

    add bash script.

---
+++ python/process.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TMPDIR=/tmp/process
+
+OUTDIR=${HOME}/Dropbox/Photos/process/Done_$(date +%Y%m%d_%H%M%S)
+
+for arg in "$@"; do
+  mkdir -p ${OUTDIR} ${TMPDIR}
+  base=`basename "${arg}" .dng`
+  echo ${base}
+  python3 ${HOME}/git/negative-film-tools/python/process.py "${arg}" "${TMPDIR}/${base}.tif"
+  convert "${TMPDIR}/${base}.tif" "${OUTDIR}/${base}.jpg"
+  exiftool -overwrite_original \
+        -TagsFromFile "${arg}" "-all:all>all:all" \
+        '-createdate<fileaccessdate' \
+        '-modifydate<fileaccessdate' \
+        '-datetimeoriginal<fileaccessdate' \
+        '-exifversion=0231' \
+        '-offsettime=+09:00' \
+        '-offsettimeoriginal=+09:00' \
+          "${OUTDIR}/${base}.jpg"
+done
+rm -rf ${TMPDIR}
