d8d587f7582d509e72d8519c6319dbf4b699f28c
Takeo Kunishima <t.kunishi@gmail.com>
2019-12-14 16:52:35 +0900

    reimplement by using skimage.

--- python/process.py
+++ python/process.py
@@ -1,66 +1,42 @@
 #!/usr/bin/python
 import os, sys
 import rawpy
-import imageio
-from PIL import Image
-import PIL.ImageOps
 import cv2
 import numpy as np
-
-def gamma(gamma_r, gamma_g, gamma_b, gain_r=1.0, gain_g=1.0, gain_b=1.0):
-    r_tbl = [min(255, int((x / 255.) ** (1. / gamma_r) * gain_r * 255.)) for x in range(256)]
-    g_tbl = [min(255, int((x / 255.) ** (1. / gamma_g) * gain_g * 255.)) for x in range(256)]
-    b_tbl = [min(255, int((x / 255.) ** (1. / gamma_b) * gain_b * 255.)) for x in range(256)]
-    return r_tbl + g_tbl + b_tbl
-
-def cv2_gamma(gamma, bit=8):
-    table = np.empty((1, 2 ** bit), np.uint16)
-    for i in range(2 ** bit):
-        table[0, i] = np.clip(pow(i / float(2 ** bit), gamma) * float(2 ** bit), 0, 2 ** bit)
-    return table
+from skimage import color, exposure, io, util
 
 raw = rawpy.imread(sys.argv[1])
 rgb = raw.postprocess(gamma=(1, 1), no_auto_bright=False, use_camera_wb=False, use_auto_wb=True, output_bps=16)
 
 # read image from array
 #image = Image.fromarray(rgb)
-image = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+#image = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+image = rgb.copy()
 
 # invert image
 #imageBox = image.getbbox()
 #cropped = image.crop(imageBox)
 #inverted = PIL.ImageOps.invert(cropped)
-inverted = cv2.bitwise_not(image)
+#inverted = cv2.bitwise_not(image)
 #inverted = rgb
+inverted = util.invert(image)
 
-# gamma correction
-GAMMA = 1.2
-#GAMMA = 1.0 
-#gamma_img = contrasted.point(gamma(GAMMA, GAMMA, GAMMA))
-#gamma_img.save('out.tif')
-#rgb_gamma = cv2.LUT(inverted, cv2_gamma(GAMMA))
-lut_table = cv2_gamma(GAMMA, bit=16)
-rgb_gamma = np.empty_like(inverted)
-rangex, rangey, dim = rgb_gamma.shape
-for x in range(rangex):
-    for y in range(rangey):
-        rgb_gamma[x, y] = lut_table[0, inverted[x, y]]
-#rgb_gamma = inverted
-
-# auto contrast
-if len(rgb_gamma.shape) == 3:
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    for channel in range(0, 3):
-        rgb_gamma[:, :, channel] = clahe.apply(rgb_gamma[:, :, channel])
+# gamma correction and auto contrast
+if len(image.shape) == 3:
+    rgb_gamma = exposure.adjust_gamma(inverted, gamma=1.8)
+    r, g, b = cv2.split(rgb_gamma)
+    r_c = exposure.rescale_intensity(r)
+    g_c = exposure.rescale_intensity(g)
+    b_c = exposure.rescale_intensity(b)
+    contrasted = cv2.merge((r_c, g_c, b_c))
 else:
-    #rgb_gamma[:, :, 0] = clahe.apply(rgb_gamma[:, :, 0])
-    rgb_gamma[:, :, 0] = cv2.equalizeHist(rgb_gamma[:, :, 0])
-contrasted = rgb_gamma
+    rgb_gamma = exposure.adjust_gamma(inverted, gamma=2.8)
+    contrasted = exposure.equalize_hist(rgb_gamma)
+    #contrasted = rgb_gamma
 
 result = contrasted
-height = result.shape[0]
-width = result.shape[1]
-cv2.imwrite(sys.argv[2], result)
-#cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
-#cv2.imshow('image', cv2.resize(result, (width // 4, height // 4)))
-#cv2.waitKey(0)
+if len(sys.argv) == 2:
+    io.imshow((result / 255).astype('uint8'))
+    io.show()
+elif len(sys.argv) == 3:
+    io.imsave(os.path.abspath(sys.argv[2]), result)
