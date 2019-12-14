d596178e8b8cdc0afdda19c91f85659f594b654e
Takeo Kunishima <t.kunishi@gmail.com>
2019-12-14 21:13:51 +0900

    options for bash script.

--- python/process.bash
+++ python/process.bash
@@ -4,16 +4,22 @@ TMPDIR=/tmp/process
 
 OUTDIR=${HOME}/Dropbox/Photos/process/Done_$(date +%Y%m%d_%H%M%S)
 
-if [ "$1" = "--bw" ]; then
-  BW="$1"
-  shift
-fi
 
 for arg in "$@"; do
+  if [[ "${arg}" == "--bw" ]]; then
+    BW="${arg}"
+    shift
+    continue
+  elif [[ "${arg}" == "--gamma="* ]]; then
+    shift
+    GAMMA=`echo "${arg}" | sed -e 's/=/ /'`
+    shift
+    continue
+  fi
   mkdir -p ${OUTDIR} ${TMPDIR}
   base=`basename "${arg}" .dng`
   echo ${base}
-  python3 ${HOME}/git/negative-film-tools/python/process.py ${BW} --out "${TMPDIR}/${base}.tif" "${arg}"
+  python3 ${HOME}/git/negative-film-tools/python/process.py ${BW} ${GAMMA} --out "${TMPDIR}/${base}.tif" "${arg}"
   convert "${TMPDIR}/${base}.tif" "${OUTDIR}/${base}.jpg"
   exiftool -overwrite_original \
         -TagsFromFile "${arg}" "-all:all>all:all" \
