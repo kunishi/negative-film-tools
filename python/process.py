fa11d27fb5800f5f1ae419d92a69e8c4a848fc44
Takeo Kunishima <t.kunishi@gmail.com>
2019-12-16 01:47:32 +0900

    reimplement auto contrast procedure.

--- python/process.py
+++ python/process.py
@@ -9,43 +9,26 @@ from skimage import color, exposure, io, util
 parser = argparse.ArgumentParser()
 parser.add_argument("src", help="source RAW file")
 parser.add_argument("--bw", help="run in bw mode", action="store_true")
-parser.add_argument("--gamma", help="specify gamma value", type=float, default=1.8)
+parser.add_argument("--gamma", help="specify gamma value", type=float, default=1.0)
 parser.add_argument("--out", help="specify the destination TIFF file")
 args = parser.parse_args()
 
 raw = rawpy.imread(args.src)
-rgb = raw.postprocess(gamma=(1, 1), no_auto_bright=False, use_camera_wb=False, use_auto_wb=True, output_bps=16)
-
-# read image from array
-#image = Image.fromarray(rgb)
-#image = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
-image = rgb.copy()
-
-# invert image
-#imageBox = image.getbbox()
-#cropped = image.crop(imageBox)
-#inverted = PIL.ImageOps.invert(cropped)
-#inverted = cv2.bitwise_not(image)
-#inverted = rgb
-inverted = util.invert(image)
+rgb = raw.postprocess(no_auto_bright=False, use_camera_wb=False, use_auto_wb=True, output_bps=16)
 
 # gamma correction and auto contrast
-if args.bw:
-    if image.shape[2] == 3:
-        bw = color.rgb2grey(inverted)
-    else:
-        bw = inverted.copy()
-    rgb_gamma = exposure.adjust_gamma(bw, gamma=1)
-    img_c = exposure.rescale_intensity(rgb_gamma)
-    contrasted = util.invert(exposure.adjust_gamma(util.invert(img_c), gamma=1/args.gamma))
-else:
-    rgb_gamma = exposure.adjust_gamma(inverted, gamma=args.gamma)
-    r, g, b = cv2.split(rgb_gamma)
+if rgb.shape[2] == 1:
+    img_c = exposure.rescale_intensity(gamma)
+else:   # color, rgb.shape[2] == 3
+    r, g, b = cv2.split(rgb)
     r_c = exposure.rescale_intensity(r)
     g_c = exposure.rescale_intensity(g)
     b_c = exposure.rescale_intensity(b)
-    img_c = cv2.merge((r_c, g_c, b_c))
-    contrasted = img_c
+    if args.bw:
+        img_c = color.rgb2grey(cv2.merge((r_c, g_c, b_c)))
+    else:
+        img_c = cv2.merge((r_c, g_c, b_c))
+contrasted = exposure.adjust_gamma(util.invert(img_c), gamma=args.gamma)
 
 result = contrasted
 if args.out:
