1a18f756c8303bcbc228eb66f2d93ff1086b98fd
Takeo Kunishima <t.kunishi@gmail.com>
2019-12-17 00:38:39 +0900

    now almost fixed the algorithm. Yay!

--- python/process.py
+++ python/process.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("src", help="source RAW file")
 parser.add_argument("--bw", help="run in bw mode", action="store_true")
 parser.add_argument("--gamma", help="specify gamma value", type=float, default=1.0)
+parser.add_argument("--hsv", help="CLAHE on HSV mode", action="store_true")
 parser.add_argument("--out", help="specify the destination TIFF file")
 args = parser.parse_args()
 
@@ -18,19 +19,27 @@ rgb = raw.postprocess(no_auto_bright=False, use_camera_wb=False, use_auto_wb=Tru
 
 # gamma correction and auto contrast
 if rgb.shape[2] == 1:
-    img_c = exposure.rescale_intensity(gamma)
+    args.bw == True
+    img_src = rgb
 else:   # color, rgb.shape[2] == 3
-    r, g, b = cv2.split(rgb)
-    r_c = exposure.rescale_intensity(r)
-    g_c = exposure.rescale_intensity(g)
-    b_c = exposure.rescale_intensity(b)
     if args.bw:
-        img_c = color.rgb2grey(cv2.merge((r_c, g_c, b_c)))
+        img_src = color.rgb2gray(rgb)
     else:
-        img_c = cv2.merge((r_c, g_c, b_c))
-contrasted = exposure.adjust_gamma(util.invert(img_c), gamma=args.gamma)
+        img_src = rgb
 
-result = contrasted
+if args.bw:
+    contrasted = exposure.equalize_adapthist(img_src, clip_limit = 0.004, kernel_size = 96)
+elif args.hsv:
+    contrasted = exposure.equalize_adapthist(img_src, clip_limit = 0.004, kernel_size = 96)
+else:
+    r, g, b = cv2.split(img_src)
+    r_c = exposure.equalize_adapthist(r, clip_limit = 0.004, kernel_size = 96)
+    g_c = exposure.equalize_adapthist(g, clip_limit = 0.004, kernel_size = 96)
+    b_c = exposure.equalize_adapthist(b, clip_limit = 0.004, kernel_size = 96)
+    contrasted = cv2.merge((r_c, g_c, b_c))
+
+result = exposure.adjust_gamma(util.invert(contrasted), gamma=args.gamma)
+
 if args.out:
     io.imsave(os.path.abspath(args.out), result)
 else:
