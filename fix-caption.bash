b9f27a6544c7ef00c10d10b1339a1ad5f2381312
Takeo Kunishima <t.kunishi@gmail.com>
2020-06-27 06:53:37 +0900

    set multiple keywords correctly

--- fix-caption.bash
+++ fix-caption.bash
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 CAPTION="$1"; shift
+
+IFS_bak=$IFS
+IFS=','
+KWOPTS=()
+for kw in ${CAPTION}; do
+  echo ${kw## }
+  KWOPTS+=("-iptc:keywords=${kw## }")
+done
+
 for file in "$@"; do
   echo "$file"
   exiftool -verbose -overwrite_original_in_place \
-    '-iptc:caption-abstract='"${CAPTION}" \
-    '-iptc:keywords='"${CAPTION}" \
-    '-subject='"${CAPTION}" \
+    "-iptc:caption-abstract=${CAPTION}" \
+    "-subject=${CAPTION}" \
+    ${KWOPTS[@]} \
     "$file"
 done
