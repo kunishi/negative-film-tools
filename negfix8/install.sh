b4948c96b748d6a2a14412ea5828389c1d0d4a07
Takeo Kunishima <t.kunishi@gmail.com>
2020-12-23 20:17:44 +0900

    remove negfix8 related tools

--- negfix8/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-scripts="autotone autowhite"
-
-for file in $scripts; do
-  curl -o $file "http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=$file&dirname=$file" && \
-    install -c -m 755 $file /usr/local/bin && \
-    rm -f $file
-done
