cce0b015e1ef00ffa6096cd998a5dcc0de43106d
Takeo Kunishima <t.kunishi@gmail.com>
2022-07-21 19:16:29 +0900

    fix dcraw-cn.bash

--- dcraw-cn.bash
+++ dcraw-cn.bash
@@ -6,15 +6,21 @@ DST=`basename "${SRC}" .dng`.jpg
 
 [ ! -d "${DSTDIR}" ] && mkdir -p "${DSTDIR}"
 cd "${DSTDIR}" && \
-	dcraw -v -c -o 0 -6 -a -H 2 -T -q 1 "${SRC}" | \
+	dcraw -v -c -o 0 -6 -g 1 1 -a -H 2 -T -q 1 "${SRC}" | \
 	magick tif:- \
+		-set colorspace rgb \
 		-define quantum:format=floating-point \
 		-depth 16 \
 		-shave 20x20 \
-		-colorspace srgb \
+		-channel rgb \
+		-auto-gamma \
+		-white-balance \
+		-auto-level \
+		-modulate 100,120,100 \
 		-negate \
-		-channel rgb,sync \
+		-colorspace srgb \
 		-normalize \
+		-white-balance \
 		"${DST}" && \
 exiftool -overwrite_original_in_place -TagsFromFile "${SRC}" \
 	'-all:all>all:all' '-orientation#=1' "${DST}"
