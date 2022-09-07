a96215ccbd610cc2f798bf335db2a7e6ee1e60fc
Takeo Kunishima <t.kunishi@gmail.com>
2022-09-08 08:54:18 +0900

    dcraw-cn 2022/09/08

--- dcraw-cn.bash
+++ dcraw-cn.bash
@@ -6,18 +6,22 @@ DST=`basename "${SRC}" .dng`.jpg
 
 [ ! -d "${DSTDIR}" ] && mkdir -p "${DSTDIR}"
 cd "${DSTDIR}" && \
-	dcraw -v -c -o 0 -6 -W -g 2.4 12.92 -H 1 -T -q 3 "${SRC}" | \
+	dcraw -v -c -o 0 -6 -g 1 1 -H 1 -T -q 3 "${SRC}" | \
 	magick tif:- \
 		-define quantum:format=floating-point \
 		-depth 32 \
 		-shave 20x20 \
-		-channel rgb,sync \
+		-colorspace srgb \
 		-auto-gamma \
 		-negate \
-		-contrast-stretch 0%x0.2% \
-		-gamma 0.8 \
+		-channel rgb \
+		-auto-gamma \
+		-channel rgb,sync \
 		-white-balance \
+		-auto-gamma \
+		-linear-stretch 0.01%x0.01% \
 		-modulate 100,150,100 \
+		-contrast-stretch 0.05%x0.05% \
 		"${DST}" && \
 exiftool -overwrite_original_in_place -TagsFromFile "${SRC}" \
 	'-all:all>all:all' '-orientation#=1' "${DST}"
