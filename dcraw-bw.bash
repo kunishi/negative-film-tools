9c92e646e91b9a22b27c085e00ee96c62e202738
Takeo Kunishima <t.kunishi@gmail.com>
2022-11-25 23:36:07 +0900

    tune up bw parameters

--- dcraw-bw.bash
+++ dcraw-bw.bash
@@ -9,15 +9,21 @@ cd "${DSTDIR}" && \
 	dcraw -v -c -o 0 -6 -g 1 1 -H 1 -T -d -q 3 "${SRC}" | \
 	magick tif:- \
 		-define quantum:format=floating-point \
-		-depth 16 \
+		-depth 32 \
 		-shave 20x20 \
 		-colorspace srgb \
 		-auto-gamma \
 		-negate \
+		-channel rgb \
+		-auto-gamma \
+		-channel rgb,sync \
+		-white-balance \
+		-auto-gamma \
+		-gamma 1.3 \
 		-colorspace LinearGray \
 		-auto-level \
 		-linear-stretch 0.01%x0.05% \
-		+level 8,98% \
+		+level 5,98% \
 		"${DST}" && \
 exiftool -overwrite_original_in_place -TagsFromFile "${SRC}" \
 	'-all:all>all:all' '-orientation#=1' "${DST}"
