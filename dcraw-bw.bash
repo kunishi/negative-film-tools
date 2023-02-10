d3bcdab73fb893d94cb312e5e7391c448ff2e8f5
Takeo Kunishima <t.kunishi@gmail.com>
2023-02-10 18:16:54 +0900

    a new method mainly using sigmoidal contrast

--- dcraw-bw.bash
+++ dcraw-bw.bash
@@ -6,22 +6,23 @@ DST=`basename "${SRC}" .dng`.jpg
 
 [ ! -d "${DSTDIR}" ] && mkdir -p "${DSTDIR}"
 cd "${DSTDIR}" && \
-	dcraw -v -c -o 0 -6 -g 1 1 -H 1 -T -d -q 3 "${SRC}" | \
+	dcraw -v -c -o 0 -6 -g 1 1 -W -T -d -q 3 "${SRC}" | \
 	magick tif:- \
 		-define quantum:format=floating-point \
 		-depth 32 \
 		-shave 20x20 \
-		-colorspace srgb \
+		-auto-gamma \
+		-auto-level \
 		-auto-gamma \
 		-negate \
-		-channel rgb \
-		-auto-gamma \
-		-channel rgb,sync \
-		-white-balance \
-		-auto-gamma \
+		-colorspace lineargray \
+		-linear-stretch 0%x0.05% \
+		+sigmoidal-contrast 2,50% \
+		+sigmoidal-contrast 2,50% \
 		-gamma 1.3 \
-		-colorspace LinearGray \
-		-linear-stretch 0.005%x0.5% \
+		-linear-stretch 0.01%x0% \
+		+level 1,96% \
+		-sigmoidal-contrast 3,20% \
 		"${DST}" && \
 exiftool -overwrite_original_in_place -TagsFromFile "${SRC}" \
 	'-all:all>all:all' '-orientation#=1' "${DST}"
