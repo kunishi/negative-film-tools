f5d79538b1784b9eb2e788371ad0715a7500c328
Takeo Kunishima <t.kunishi@gmail.com>
2023-02-16 09:04:58 +0900

    readjust cn parameters based on sigmoidal contrast

--- dcraw-cn.bash
+++ dcraw-cn.bash
@@ -6,19 +6,24 @@ DST=`basename "${SRC}" .dng`.jpg
 
 [ ! -d "${DSTDIR}" ] && mkdir -p "${DSTDIR}"
 cd "${DSTDIR}" && \
-	dcraw -v -c -o 0 -6 -g 1 1 -H 1 -T -q 3 "${SRC}" | \
+	dcraw -v -c -o 0 -6 -g 1 1 -W -T -q 3 "${SRC}" | \
 	magick tif:- \
 		-define quantum:format=floating-point \
 		-depth 32 \
 		-shave 20x20 \
 		-set colorspace srgb \
-		-negate \
 		-colorspace rgb \
+		-white-balance \
+		-modulate 100,105,100 \
+		-negate \
 		-channel rgb,sync \
+		-sigmoidal-contrast 3,50% \
 		-auto-gamma \
-		-contrast-stretch 0%x0% \
 		-modulate 100,130,100 \
-		-normalize \
+		-contrast-stretch 0.03%x0.07% \
+		+sigmoidal-contrast 2,10% \
+		-gamma 0.8 \
+		+level 10%,95% \
 		"${DST}" && \
 exiftool -overwrite_original_in_place -TagsFromFile "${SRC}" \
 	'-all:all>all:all' '-orientation#=1' "${DST}"
