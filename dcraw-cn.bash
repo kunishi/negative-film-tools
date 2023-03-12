9f99453c6adce5bb8e56f8d8e7e77ae042b45a51
Takeo Kunishima <t.kunishi@gmail.com>
2023-03-12 23:09:48 +0900

    parameters for color negatives 20230312.

--- dcraw-cn.bash
+++ dcraw-cn.bash
@@ -6,25 +6,25 @@ DST=`basename "${SRC}" .dng`.heic
 
 [ ! -d "${DSTDIR}" ] && mkdir -p "${DSTDIR}"
 cd "${DSTDIR}" && \
-	dcraw -v -c -o 0 -6 -g 1 1 -W -T -q 3 "${SRC}" | \
+	dcraw -v -c -o 0 -6 -g 1 1 -H 5 -T -q 3 "${SRC}" | \
 	magick tif:- \
 		-depth 16 \
 		-shave 20x20 \
 		-set colorspace srgb \
-		-white-balance \
 		-modulate 100,105,100 \
 		-negate \
 		-channel rgb,sync \
 		-sigmoidal-contrast 3,50% \
 		-auto-gamma \
 		-modulate 100,125,100 \
-		-contrast-stretch 0.03%x0.07% \
 		+sigmoidal-contrast 2,10% \
+		-linear-stretch 0%x0.04% \
 		-colorspace lab \
 		-channel red \
-		-auto-level \
-		+channel \
+		-sigmoidal-contrast 3,50% \
+		-channel rgb,sync \
 		-colorspace srgb \
+		-contrast-stretch 0%x0.02% \
 		"${DST}" && \
 exiftool -overwrite_original_in_place -TagsFromFile "${SRC}" \
 	'-all:all>all:all' '-orientation#=1' "${DST}"
