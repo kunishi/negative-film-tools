94a60253f39730217769f9aa1a5eff778ee173a1
Takeo Kunishima <t.kunishi@gmail.com>
2023-04-09 17:56:36 +0900

    simplified cn params

--- dcraw-cn.bash
+++ dcraw-cn.bash
@@ -11,13 +11,13 @@ cd "${DSTDIR}" && \
 		-depth 16 \
 		-shave 20x20 \
 		-set colorspace srgb \
-		-channel rgb -auto-gamma -channel rgb,sync \
-		-contrast-stretch 0.01%x0% \
-		-linear-stretch 0%x0.03% \
-		-sigmoidal-contrast 3,50% \
+		-channel rgb,!sync -auto-gamma -channel rgb,sync \
+		-gamma 2.4 \
+		-auto-level \
+		-contrast-stretch 0.3%,0% \
+		-linear-stretch 0%,0.2% \
 		-negate \
-		-gamma 1.3 \
-		-modulate 100,115,100 \
+		-modulate 100,130,100 \
 		"${DST}" && \
 exiftool -overwrite_original_in_place -TagsFromFile "${SRC}" \
 	'-all:all>all:all' '-orientation#=1' "${DST}"
